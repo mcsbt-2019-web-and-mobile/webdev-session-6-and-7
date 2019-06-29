f86e42f48c24ebb36e4e636a37432c42ca5534d6
Pepe <pepe@pepegar.com>
2019-06-29 02:31:36 +0200

    exercise2 solved

--- exercises/exercise2/src/index.js
+++ exercises/exercise2/src/index.js
@@ -5,72 +5,24 @@ import './index.css';
 
 const Post = props => {
   let link = `/post/${props.id}`
-  return <li className="list-group-item"><Link to={link}>{props.title}</Link></li>
+  return <li className="list-group-item"><a href={link}>{props.title}</a></li>
 };
 
 class Sidebar extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      posts: []
-    };
-  }
-
-  componentDidMount() {
-    fetch("http://hn.algolia.com/api/v1/search?query=React")
-      .then(resp => resp.json())
-      .then(json => this.setState({
-        posts: json.hits
-      }));
-  }
-  
   render() {
-    let posts = this.state.posts.map(post => <Post id={post.objectID} title={post.title}/>);
     return <div className="sidebar">
       <ul className="list-group">
-        {posts}
+        <Post id="" title="first post"/>
       </ul>
     </div>;
   }
 }
 
 class Article extends React.Component {
-
-  constructor() {
-    super();
-    this.state = {
-      data: null
-    };
-  }
-
-  componentWillReceiveProps() {
-    fetch(`http://hn.algolia.com/api/v1/items/${this.props.match.params.id}`)
-      .then(resp => resp.json())
-      .then(json => this.setState({
-        data: json
-      }));
-  }
-
-  componentDidMount() {
-    fetch(`http://hn.algolia.com/api/v1/items/${this.props.match.params.id}`)
-      .then(resp => resp.json())
-      .then(json => this.setState({
-        data: json
-      }));
-  }
-  
   render() {
-    if (this.state.data) {
-      let children = this.state.data.children.map(c => c.text);
-      return (
-        <div>
-          <h1>{this.state.data.title}</h1>
-          {children}
-        </div>
-      );
-    } else {
-      return <h1></h1>
-    }
+    return <div>
+      <h1>this is the title</h1>
+    </div>
   }
 }
 
@@ -78,8 +30,6 @@ const Home = props => (<h1>HOME</h1>);
 
 const Main = props => (
   <div className="main">
-    <Route exact path="/" component={Home}/>
-    <Route path="/post/:id" component={Article}/>    
   </div>
 );
 
@@ -90,4 +40,4 @@ const App = props => (
   </div>
 );
 
-ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
